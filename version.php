823203d25f0d44b393d74d3d34f1f00536d7f432
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-04 12:29:46 -0300

    V 1.3.8

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026082900;
-$plugin->release = "1.3.7";
+$plugin->version = 2026090400;
+$plugin->release = "1.3.8";
 $plugin->component = "mod_videoprogress";
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
